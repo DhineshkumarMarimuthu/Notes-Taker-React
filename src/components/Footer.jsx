731ed0053a9787de4e0bb33efe92410b6359619b
Dhineshkumar M <dhineshkumarmarimuthu2k@gmail.com>
2021-06-07 13:22:19 +0530

    deploy my react app to github pages

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
+
+export default function Footer(){
+  return (
+    <footer>
+      <p>Copyright ⓒ {currentYear}</p>
+    </footer>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import HighlightIcon from '@material-ui/icons/Highlight';
+import React from "react";
+export default function Header(){
+  return(
+    <header>
+      <h1><HighlightIcon />Keeper</h1>
+    </header>
+  );
+}
